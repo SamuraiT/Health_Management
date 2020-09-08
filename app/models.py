9515c88309e260c62a07d88d0e825c024fcb3e94
samurait <t.yasukawa01@gmail.com>
2020-09-08 23:00:54 +0900

    Add user as foreign key

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.contrib.auth import get_user_model
 EXERCISE = (('N/A', 'Exercise'),('Yes', 'Yes'), ('No', 'No'))
 
 class HealthApp(models.Model):
@@ -9,7 +9,10 @@ class HealthApp(models.Model):
     steps = models.IntegerField()
     exercise = models.CharField(max_length=10, choices=EXERCISE)
     notes = models.TextField(max_length=100, null=True, blank=True)
-
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.SET_NULL,
+        null=True
+    )
     def __str__(self):
         return str(self.postdate)
-    
