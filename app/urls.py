a9bed211e3b49c25ae91320ef377039b25f38107
kyosuke0602 <kkusaka6_2@yahoo.co.jp>
2020-09-12 16:53:36 +0900

    updated

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from .views import homeview, create, delete, get_svg
 
 urlpatterns = [
     path('', homeview, name='home'),
+    path('<int:month>/<int:year>', homeview, name='home'),
     path('create/', create, name='create'),
     path('<int:id>/delete', delete, name='delete'),
     path('plot/', get_svg, name='plot'),
